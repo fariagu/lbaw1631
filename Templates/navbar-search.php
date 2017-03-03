c9f544e8f1f2d327b6629593343ad6a120fb9564
fariagu <gustavoduartefaria@gmail.com>
2017-03-03 16:36:07 +0000

    unfinished mobile navbar

--- Templates/navbar-search.php
+++ Templates/navbar-search.php
@@ -1,6 +1,6 @@
-<form class="navbar-form navbar-right">
+<form class="navbar-form navbar-right" data-toggle="collapse" data-target=".navbar-collapse.in">
 	<div class="form-group">
-		<input type="text" class="form-control" placeholder="Search">
+		<input type="text" class="form-control" placeholder="Search" >
 	</div>
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
--- Templates/navbar.php
+++ Templates/navbar.php
@@ -3,8 +3,8 @@
 		<?php include('navbar-left.php'); ?>
 		
 		<ul class="nav navbar-nav navbar-right">
-			<li><a href="../register"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
-			<li><a href="../login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+			<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../register"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+			<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
 		</ul>
 		
 		<?php include('navbar-search.php'); ?>
@@ -15,12 +15,15 @@
 		<?php include('navbar-left.php'); ?>
 		
 		<ul class="nav navbar-nav">
-		  <li><a href="">Home</a></li>
-		  <li><a href="../post">Post a question</a></li>
+		  <li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="">Home</a></li>
+		  <li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../post">Post a question</a></li>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
-			<li><a href="../profile"><span class="glyphicon glyphicon-user"></span>Profile</a></li>
-			<li><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+			<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../profile">
+                    <span class="glyphicon glyphicon-user"></span>Profile
+                </a>
+            </li>
+			<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
 		</ul>
 		
 		<?php include('navbar-search.php'); ?>
