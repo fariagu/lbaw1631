9c2779cf18eb37b4379d445d1143b631f0fde0f9
Daniel Fernandes <Daniel Fernandes>
2017-03-04 16:16:00 +0000

    Added email

--- Templates/register-form.php
+++ Templates/register-form.php
@@ -19,6 +19,12 @@
                 <input type="password" class="form-control" id="pwd2" placeholder="Enter password">
             </div>
       </div>
+	  <div class="form-group">
+            <label class="control-label col-sm-2" for="email">E-mail:</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="email" placeholder="Enter email">
+            </div>
+      </div>
       <div class="form-group">
             <label class="control-label col-sm-2" for="firstName">First name:</label>
             <div class="col-sm-10">
