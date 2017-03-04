719b50e3867577aa5e7847cb0efaeb87cf340a79
fariagu <gustavoduartefaria@gmail.com>
2017-03-04 09:22:36 +0000

    changed links

--- Templates/navbar-center.php
+++ Templates/navbar-center.php
@@ -1,5 +1,5 @@
 <ul class="nav navbar-nav">
-	<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="/home">Home</a></li>
+	<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../home">Home</a></li>
 	<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../post">Post a question</a></li>
 </ul>
 <ul class="nav navbar-nav navbar-right">
--- all/users/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('../../Templates/head.php');
-include('../../Templates/navbar.php');
-include('../../Templates/sideNavbar.php');
-include('../../Templates/all-users.php');
-include('../../Templates/footer.php');
-
-?>
