d2301ad3de5f0b94f11ee50459cfbaba50a9db83
Daniel Fernandes <Daniel Fernandes>
2017-03-04 14:57:23 +0000

    Changed Topics to Categories

--- Templates/navbar-left.php
+++ Templates/navbar-left.php
@@ -6,7 +6,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#menu-toggle" id="menu-toggle">Topics</a>
+            <a class="navbar-brand" href="#menu-toggle" id="menu-toggle">Categories</a>
         </div>
             
         <div class="collapse navbar-collapse">
