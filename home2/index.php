b21e9d43bbf303216580e2b177b6daac964ceb66
Daniel Fernandes <Daniel Fernandes>
2017-03-04 21:53:13 +0000

    Added temporary pages

---
+++ home2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include('../Templates/head.php');
+include('../Templates/navbar2.php');
+include('../Templates/sideNavbar.php');
+include('../Templates/home-page.php');
+include('../Templates/footer.php');
+
+?>
---
+++ home3/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include('../Templates/head.php');
+include('../Templates/navbar3.php');
+include('../Templates/sideNavbar.php');
+include('../Templates/home-page.php');
+include('../Templates/footer.php');
+
+?>
