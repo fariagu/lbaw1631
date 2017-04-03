268b8a19d3d881e6af2da65f2447c2bc576f6a67
Gustavo Faria <gustavoduartefaria@gmail.com>
2017-04-03 08:50:57 +0100

    Config changed

--- frmk/config/init.php
+++ frmk/config/init.php
@@ -1,13 +1,13 @@
 <?php
-  session_set_cookie_params(3600, '/~lbaw16xx'); //FIXME
+  session_set_cookie_params(3600, '/~lbaw1631'); //FIXME
   session_start();
 
   error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-  $BASE_DIR = '/opt/lbaw/lbaw16xx/public_html/frmk/'; //FIXME
-  $BASE_URL = '/~lbaw16xx/frmk/'; //FIXME
+  $BASE_DIR = '/opt/lbaw/lbaw1631/public_html/frmk/'; //FIXME
+  $BASE_URL = '/~lbaw1631/frmk/'; //FIXME
 
-  $conn = new PDO('pgsql:host=dbm;dbname=lbaw16xx', 'lbaw16xx', '_password_'); //FIXME
+  $conn = new PDO('pgsql:host=dbm;dbname=lbaw1631', 'lbaw1631', 'iy55xp91'); //FIXME
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
