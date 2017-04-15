ef68fc8b213281b4492e693d9a56048bb3dc938a
Gustavo Faria <gustavoduartefaria@gmail.com>
2017-04-15 12:46:13 +0100

    all-reports

--- proto/templates_c/2b49123582b74970c65a355c285662aca0e63c89.file.navbar-admin.tpl.php
+++ proto/templates_c/2b49123582b74970c65a355c285662aca0e63c89.file.navbar-admin.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2017-04-15 11:31:24
+<?php /* Smarty version Smarty-3.1.15, created on 2017-04-15 12:45:25
          compiled from "/mnt/d/CODE/www/lbaw1631/proto/templates/common/navbar-admin.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:135196449058f1f67c9bbba0-14649249%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '2b49123582b74970c65a355c285662aca0e63c89' => 
     array (
       0 => '/mnt/d/CODE/www/lbaw1631/proto/templates/common/navbar-admin.tpl',
-      1 => 1492183391,
+      1 => 1492256513,
       2 => 'file',
     ),
   ),
@@ -15,10 +15,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.15',
   'unifunc' => 'content_58f1f67c9bdd27_67195846',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_58f1f67c9bdd27_67195846')) {function content_58f1f67c9bdd27_67195846($_smarty_tpl) {?><ul class="nav navbar-nav">
-	<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../admin">Administration</a></li>
+	<li><a data-toggle="collapse" data-target=".navbar-collapse.in" href="../admin/reports.php">Administration</a></li>
 </ul><?php }} ?>
