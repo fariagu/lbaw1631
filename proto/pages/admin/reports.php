e4b96050a977812fb8ed65e61406934c0327e056
unknown <daplf96@gmail.com>
2017-04-15 13:56:43 +0100

    Quick fixes

--- proto/pages/admin/reports.php
+++ proto/pages/admin/reports.php
@@ -2,6 +2,12 @@
   include_once('../../config/init.php');
   include_once($BASE_DIR .'database/category.php');
   
+  if(!$_SESSION['admin'])
+  {
+	  header("Location: $BASE_URL");
+	  exit;
+  }
+  
   $top_categories = getTopCategories();
   
   $smarty->assign('top_categories', $top_categories);
