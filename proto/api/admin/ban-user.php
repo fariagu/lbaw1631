5361377109de34bc97debc23873b2b898df90e44
unknown <daplf96@gmail.com>
2017-05-29 14:10:07 +0100

    Added bans

---
+++ proto/api/admin/ban-user.php
@@ -0,0 +1,18 @@
+<?php
+  include_once('../../config/init.php');
+  include_once($BASE_DIR .'database/member.php');
+  
+  if(!$_GET['id'])
+  {
+	  http_response_code(404);
+	  exit;
+  }
+  
+  try {
+	  banUser($_GET['id']);
+	  http_response_code(200);
+  }
+  catch (PDOException $e) {
+	  http_response_code(404);
+  }
+?>
